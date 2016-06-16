95fcea7a65b1fda27f68b45980ac96fda18ec85f
Merrick Kavolsky <technicmk@gmail.com>
2016-06-16 14:11:51 +0200

    Remove non essential tables and update auth_keys schema

--- sql/init.sql
+++ sql/init.sql
@@ -2,26 +2,10 @@ drop database if exists api_db;
 create database api_db;
 use api_db;
 
-create table users(
-  id int primary key auto_increment,
-  created datetime not null default current_timestamp,
-  validated boolean not null default false,
-  email varchar(45) unique not null,
-  password varchar(64) not null,
-  auth_lvl  int not null default 1
-);
-
-create table profiles(
-  id int primary key auto_increment,
-  user_id int unique  not null references users(id),
-  name varchar(20) not null,
-  surname varchar(25) not null
-);
-
 create table auth_keys(
   id int primary key auto_increment,
   created datetime not null default current_timestamp,
-  user_id int references users(id),
+  user_id unique int references users(id),
   auth_key varchar(64) not null
 );
 
