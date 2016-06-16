afb3f7ba8eae14fbc7e397976a2ed0e5e9da962a
Merrick Kavolsky <technicmk@gmail.com>
2016-06-16 14:12:48 +0200

    Update table schema

--- sql/init.sql
+++ sql/init.sql
@@ -2,6 +2,15 @@ drop database if exists api_db;
 create database api_db;
 use api_db;
 
+create table users(
+  id int primary key auto_increment,
+  created datetime not null default current_timestamp,
+  username varchar(20) unique not null,
+  password varchar(64) not null,
+  auth_lvl  int not null default 1
+);
+
+
 create table auth_keys(
   id int primary key auto_increment,
   created datetime not null default current_timestamp,
