5993647969be6319a2d40346b21e00a17ecb70ce
Merrick Kavolsky <technicmk@gmail.com>
2016-07-25 20:00:58 +0200

    Update sql schemas to use varchar uid

--- sql/init.sql
+++ sql/init.sql
@@ -3,16 +3,17 @@ create database api_db;
 use api_db;
 
 create table users(
-  id int primary key auto_increment,
+  uid varchar(64) primary key,
   created datetime not null default current_timestamp,
   email varchar(40) unique not null,
   password varchar(64) not null,
+  validated boolean not null default false,
   user_group int not null default 1
 );
 
 
 create table auth_keys(
-  user_id int unique references users(id),
+  uid varchar(64) references users(id),
   created datetime not null default current_timestamp,
   expires datetime not null,
   auth_key varchar(64) not null
