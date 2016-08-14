25acae044a2bb3d9400408d0f94ce1b5dc758f07
Merrick Kavolsky <technicmk@gmail.com>
2016-08-14 16:06:16 +0200

    Create Model.php and migrate.php

---
+++ lib/Model.php
@@ -0,0 +1,23 @@
+<?php
+
+class Model{
+
+  private final function prepareAccessorMethods(){
+  }
+
+  private final function prepareRetrievalMethods(){
+  }
+
+  protected static final function _find(){
+  }
+
+  public save(){
+  }
+
+  public delete(){
+  }
+
+  public toJSON(){
+  }
+}
+
---
+++ migrate.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once './lib/Config.php';
+require_once './lib/DbManager.php';
+
+$dbm = new DbManager(DSN, DB_HOST, DB_NAME, DB_USER, DB_PASS);
+
+
