30ff862018dbfab62157ec7ccba80eef182b18c3
Merrick Kavolsky <technicmk@gmail.com>
2016-09-12 15:35:23 +0200

    Add admin/controller methods for OAuth2 endpoints and update user creation end point

--- admin/controllers.php
+++ admin/controllers.php
@@ -2,18 +2,26 @@
 
 class UserController extends RoutedController{
 
-  public function post_login($request, $args){
-    //TODO implement with OAuth2
-  }
-
-  public function get_logout($request, $args){
-    //TODO implement with OAuth2
-  }
-
   public function post_create($request, $args){
     $this->assertArrayKeysSet(['email', 'password'], $_POST);
     $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-    $pass = Auth::hashPassword($_POST['password']);
+    $pass = $_POST['password'];
+
+    //prevent duplicate email entries, this is already done by our DDL in
+    //the User model's meta class, but to prevent from getting nasty SQL Db
+    //exceptions for trying to enter a duplicate unique field we'll return
+    //a failed response ourselves instead of letting the API handle the
+    //exception thrown by our database so we can have a more detailed and clear
+    //failure reason
+    if(Models::fetchSingle('User', ['email'=> $email])){
+      return Response::fail('Email already in use.');
+    }
+    $user = new User([
+      'email' => $email,
+      'password' => $pass,
+      'username' => $email
+    ]);
+    $user->save();
     return Response::success();
   }
 
@@ -22,17 +30,25 @@ class UserController extends RoutedController{
     return Response::success();
   }
 
-  public function all_info($request, $arg){
-    return Response::success();
-  }
-
   public function index($request, $arg){
-    return Response::success("Nothing here");
+    $user = Auth::currentUser();
+    //return Response::success($user->toJSON());
+    return Response::success($user);
   }
 }
 
 class AuthController extends RoutedController{
 
-  public function post_deauthenticate($request, $args){
+  public function post_token($request, $args){
+    return Auth::requestToken();
+  }
+
+  public function get_revoke($request, $args){
+    return Auth::revokeToken();
+  }
+
+  public function post_revoke($request, $args){
+    return Auth::revokeToken();
   }
 }
+
