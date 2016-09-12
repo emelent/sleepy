01833c43553dcd4fb3d322d4e7ce5d1327ce7098
Merrick Kavolsky <technicmk@gmail.com>
2016-09-12 22:23:09 +0200

    Create snakeToCamel helper function

--- lib/core/utils.php
+++ lib/core/utils.php
@@ -1,8 +1,25 @@
 <?php
-  function arrayKeysSet($keys, $record){
-    foreach($keys as $key){
-        if(!isset($record[$key]))
-            return false;
-    } 
-    return true;
+
+function arrayKeysSet($keys, $record){
+  foreach($keys as $key){
+      if(!isset($record[$key]))
+          return false;
+  } 
+  return true;
+}
+
+function snakeToCamel($phrase){
+  $parts = explode('_', $phrase);
+  $newPhrase = '';
+  $first = true;
+  foreach($parts as $part){
+    if($first){
+      $first = false;
+      $newPhrase .= $part;
+      continue;
+    }
+    $newPhrase .= ucfirst($part);
   }
+  return $newPhrase;
+}
+
