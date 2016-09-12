44c0a4c5205196df6c504e8dd919f6cc6bbf6a9b
Merrick Kavolsky <technicmk@gmail.com>
2016-09-12 22:21:08 +0200

    Create ActivationCode Model

--- admin/models.php
+++ admin/models.php
@@ -10,7 +10,7 @@ class UserMeta extends ModelMeta{
       'username' => new CharField(255, ['unique' => true]),
       'password' => new CharField(255),
       'created' => new DateTimeField(['default'=> 'CURRENT_TIMESTAMP']),
-      'validated' => new BooleanField(['default'=> 'FALSE']),
+      'activated' => new BooleanField(['default'=> 'FALSE']),
       'group' => new IntegerField(['default'=> 1]),
       'first_name' => new CharField(255, ['null'=>true]),  
       'last_name' => new CharField(255, ['null'=>true]),  
@@ -24,24 +24,35 @@ class User extends Model{
   public function setPassword($pass){
     $this->password = Auth::hashPassword($pass);
   }
+
+  public function setUsername($username){
+    if(Models::fetchSingle(get_class($this), ['username'=>$username])){
+      Response::fail('Username already exists.')->unwrap();
+    }
+    $this->username = $username;  
+  }
+
+  public function setEmail($email){
+    if(Models::fetchSingle(get_class($this), ['email'=>$email])){
+      Response::fail('Email already exists.')->unwrap();
+    }
+    $this->email = $email;  
+  }
 }
 
-//AUTH META
-//class AuthMeta extends ModelMeta{
-  //public function __construct(){
-    //parent::__construct('auth', [
-      //'created' => new DateTimeField(['default'=> 'CURRENT_TIMESTAMP']),
-      //'expires' => new DateTimeField(),
-      //'token' => new CharField(64),
-      //'user_id' => new CustomField('INT REFERENCES `users`(id)'),
-      //'level' => new IntegerField(['default' => 0])
-    //]); 
-  //}
-//}
+class ActivationCodeMeta extends ModelMeta{
 
-//AUTH MODEL
-//class Auth extends Model{}
+  public function __construct(){
+    parent::__construct('activation_codes', [
+      'code' => new CharField(256),
+      'user_id' => new CustomField("INT NOT NULL REFERENCES users(id)"),
+      'expires' => new DateTimeField()
+    ]);
+  }
+}
+class ActivationCode extends Model{}
 
 //register models
 ModelManager::register('User');
+ModelManager::register('ActivationCode');
 //ModelManager::register('Auth');
