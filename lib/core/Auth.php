b004cb1dd2194b8fcf6fb91e5aaaf42bb620202a
Merrick Kavolsky <technicmk@gmail.com>
2016-09-12 15:32:17 +0200

    Update Auth methods to use OAuth2

--- lib/core/Auth.php
+++ lib/core/Auth.php
@@ -6,45 +6,102 @@ const ADMIN = 9;
 
 class Auth{
 
-  private static $user;
-  private static $auth;
-
   public static $GROUP = [
     GUEST  => 'guest',
     USER  => 'user',
     ADMIN => 'admin'
   ];
 
+  private static $server = null;
+  private static $user;
+  private static $auth;
+  private static $storage = null;
+
+  public static function configOAuth($clientID, $clientSecret, $redirectURI, $grantTypes=[]){
+    $storage = new CustomStorage(App::getPdo());
+    $storage->setClientDetails($clientID, $clientSecret, $redirectURI);
+
+    // Pass a storage object or array of storage objects to the OAuth2 server class
+    $server = new OAuth2\Server($storage);
+
+    //foreach($grantTypes as $grantType){
+      //$server->addGrantType($grantType);
+    //}
+    
+    // Add the "Client Credentials" grant type (it is the simplest of the grant types)
+    $server->addGrantType(new OAuth2\GrantType\ClientCredentials($storage));
+
+    // Add the "Authorization Code" grant type (this is where the oauth magic happens)
+    $server->addGrantType(new OAuth2\GrantType\AuthorizationCode($storage));
+
+    // Add "RefreshToken" grant type
+    $server->addGrantType(new OAuth2\GrantType\RefreshToken($storage, array(
+      'always_issue_new_refresh_token' => true)));
+
+    //Add "Password" grant type
+    $server->addGrantType(new OAuth2\GrantType\UserCredentials($storage));
+
+    Auth::$server = $server;
+    Auth::$storage = $storage;
+  }
+
+  private static function createResponseFromOAuth($response){
+    $data = $response->getParameters();
+    $statusCode = $response->getStatusCode();
+    $success = true;
+    if(isset($data['error'])){
+      return Response::fail("[OAuth] {$data['error']}", $statusCode);
+    }
+    return Response::success($data, $statusCode);
+  }
+
+  public static function getOAuthRequest(){
+    return OAuth2\Request::createFromGlobals();
+  }
+
+  //public static function addGrantType($grantType){
+    //Auth::$server->addGrantType($grantType);
+  //}
+
+  public static function requestToken(){
+    return Auth::createResponseFromOAuth(
+      Auth::$server->handleTokenRequest(Auth::getOAuthRequest())
+    );
+  }
+
+  public static function revokeToken(){
+    return Auth::createResponseFromOAuth(
+      Auth::$server->handleRevokeRequest(Auth::getOAuthRequest())
+    );
+  }
+
+  private static function requireAuthorisation(){
+    // Handle a request to a resource and authenticate the access token
+    if (!Auth::$server->verifyResourceRequest(Auth::getOAuthRequest())) {
+      Response::fail("Not authorised.")->unwrap();
+    }
+  }
+
+  public static function getTokenData(){
+    return Auth::$server->getAccessTokenData(Auth::getOAuthRequest());
+  }
+
   public static function hashPassword($password){
     return password_hash($password, PASSWORD_BCRYPT);
   }
 
-  public static function requireAuthentication(){
-    $request = Request::getInstance(); 
-    $key = $request->getApiKey();
-    
-    //TODO check key with key in db
-    //if it doesn't match, throw an exception
-
-    //TODO check if key expired
-    //throw an exception if it is and remove it from the db
-
-    //TODO update $user
-    
-    //TODO update $auth
+  public static function verifyPassword($password, $hash){
+    return password_verify($password, $hash);
   }
 
-  public static function requireAuthorisation($level){
-    requireAuthentication();
-    $auth = Auth::getAuth(); 
-    if($auth->level < $level){
-      //TODO throw exception not authorised
-    }
-  }
+  //public static function requireAuthentication(){
+    ////TODO use oauth to allow access to resource
+  //}
 
   public static function requireUserGroup($group){
-    requireAuthentication();
-    $user = Auth::getUser();
+    //TODO require user to be of certain group.. maybe this can
+    //be done with oauth
+    $user = Auth::currentUser();
     if($user->group < $group){
       //TODO throw exception restricted access
     }
@@ -56,10 +113,16 @@ class Auth{
     //}
     return Auth::$auth;
   }
-  public static function getUser(){
-    //if(Auth::$user == null){
-      ////TODO get user from db using api key
-    //}
+
+  public static function currentUser(){
+    Auth::requireAuthorisation();
+    if(Auth::$user == null){
+      //TODO get user from db using api key
+      $tokenData = Auth::getTokenData();
+      Auth::$user = Models::fetchSingle('User', [
+        'username'=> $tokenData['user_id']
+      ]);
+    }
     return Auth::$user;
   }
 }
