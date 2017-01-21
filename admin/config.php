bd6b2ffaafcb31a8a04d4a0705212f7e9a0f9ef1
Merrick Kavolsky <technicmk@gmail.com>
2017-01-21 13:06:41 +0200

    Create tables if they don't exist

--- admin/config.php
+++ admin/config.php
@@ -79,6 +79,17 @@ function checkDb(){
         CREATE TABLE oauth_jwt (client_id VARCHAR(80) NOT NULL, subject VARCHAR(80), public_key VARCHAR(2000), CONSTRAINT jwt_client_id_pk PRIMARY KEY (client_id));
       ";
       $pdo->query($query);
+    }else{
+        $pdo = new PDO("$dsn:dbname=$dbname;host=$dbhost;", $dbuser, $dbpass);
+        $query = "
+        CREATE TABLE IF NOT EXIST oauth_clients (client_id VARCHAR(80) NOT NULL, client_secret VARCHAR(80), redirect_uri VARCHAR(2000) NOT NULL, grant_types VARCHAR(80), scope VARCHAR(100), user_id VARCHAR(80), CONSTRAINT clients_client_id_pk PRIMARY KEY (client_id));
+        CREATE TABLE IF NOT EXIST oauth_access_tokens (access_token VARCHAR(40) NOT NULL, client_id VARCHAR(80) NOT NULL, user_id VARCHAR(255), expires TIMESTAMP NOT NULL, scope VARCHAR(2000), CONSTRAINT access_token_pk PRIMARY KEY (access_token));
+        CREATE TABLE IF NOT EXIST oauth_authorization_codes (authorization_code VARCHAR(40) NOT NULL, client_id VARCHAR(80) NOT NULL, user_id VARCHAR(255), redirect_uri VARCHAR(2000), expires TIMESTAMP NOT NULL, scope VARCHAR(2000), CONSTRAINT auth_code_pk PRIMARY KEY (authorization_code));
+        CREATE TABLE IF NOT EXIST oauth_refresh_tokens (refresh_token VARCHAR(40) NOT NULL, client_id VARCHAR(80) NOT NULL, user_id VARCHAR(255), expires TIMESTAMP NOT NULL, scope VARCHAR(2000), CONSTRAINT refresh_token_pk PRIMARY KEY (refresh_token));
+        CREATE TABLE IF NOT EXIST oauth_scopes (scope TEXT, is_default BOOLEAN);
+        CREATE TABLE IF NOT EXIST oauth_jwt (client_id VARCHAR(80) NOT NULL, subject VARCHAR(80), public_key VARCHAR(2000), CONSTRAINT jwt_client_id_pk PRIMARY KEY (client_id));
+      ";
+      $pdo->query($query);
     }
   }catch(Exception $e){
     echo $e->getMessage();
